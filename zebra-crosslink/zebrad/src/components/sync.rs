efa9ed44e5e5f1f8bd64e07ba97096da7bd33342
Sam H. Smith <sam.henning.smith@protonmail.com>
2025-11-29 16:20:18 +0100

    fix issue of not looking in side chains and increase sync speed

--- zebra-crosslink/zebrad/src/components/sync.rs
+++ zebra-crosslink/zebrad/src/components/sync.rs
@@ -204,7 +204,7 @@ const FINAL_CHECKPOINT_BLOCK_VERIFY_TIMEOUT_LIMIT: HeightDiff = 100;
 /// If this delay is removed (or set too low), the syncer will
 /// sometimes get stuck in a failure loop, due to leftover downloads from
 /// previous sync runs.
-const SYNC_RESTART_DELAY: Duration = Duration::from_secs(67);
+const SYNC_RESTART_DELAY: Duration = Duration::from_secs(13);
 
 /// Controls how long we wait to retry a failed attempt to download
 /// and verify the genesis block.
