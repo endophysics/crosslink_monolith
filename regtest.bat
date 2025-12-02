105c44e130ba449240bc9b2d6060b923201cce4d
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2025-12-02 20:59:23 +0400

    fix

--- regtest.bat
+++ regtest.bat
@@ -6,5 +6,5 @@ set "PROTOC=C:\Work\ShieldedLabs\protoc-33.1-win64\bin\protoc.exe"
 cd C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\
 cargo build -Fviz_gui
 
-start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "regtestnet_1.toml"
-start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "regtestnet_2.toml"
+start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "testnet_1.toml"
+start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "testnet_2.toml"
