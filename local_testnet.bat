4bbf08320774256bd0bd1b1a09d2e8a84a5ae426
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2025-12-03 03:04:39 +0400

    Misc: build flow changes

--- local_testnet.bat
+++ local_testnet.bat
@@ -7,4 +7,8 @@ set "PROTOC=C:\Work\ShieldedLabs\protoc-33.1-win64\bin\protoc.exe"
 pushd C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\
 cargo build -Fviz_gui
 REM && start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "testnet_1.toml" && start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "testnet_2.toml"
+
+REM "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\testnet_1.toml"
+REM "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\testnet_2.toml"
+
 popd
