128b3e2b96a30b94afd7e6b3211a3c35b40b81f5
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2025-12-02 20:19:30 +0400

    regtestnet

---
+++ regtest.bat
@@ -0,0 +1,10 @@
+
+@echo off
+
+set "PROTOC=C:\Work\ShieldedLabs\protoc-33.1-win64\bin\protoc.exe"
+
+cd C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\
+REM cargo build -Fviz_gui
+
+start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "regtestnet_1.toml"
+start "" "C:\Work\ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps\zebrad.exe" "-c" "regtestnet_2.toml"
