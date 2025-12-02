28642a5c05c2713021e6934f88fdb00fd98a8165
Judah <judahrcr@gmail.com>
2025-12-02 07:44:23 -0700

    more things

--- zebra-gui/src/main.rs
+++ zebra-gui/src/main.rs
@@ -16,6 +16,13 @@ fn main() {
         wallet_state.lock().unwrap().roster = vec![
             RosterMember{ pub_key: [0xAAu8; 32], stake: 250_000_000 },
             RosterMember{ pub_key: [0xBBu8; 32], stake: 100_000_000 },
+            RosterMember{ pub_key: [0xCCu8; 32], stake: 300_000_000 },
+            RosterMember{ pub_key: [0xDDu8; 32], stake: 500_000_000 },
+            RosterMember{ pub_key: [0xEEu8; 32], stake: 500_555_000 },
+            RosterMember{ pub_key: [0xFFu8; 32], stake: 000_000_000 },
+            RosterMember{ pub_key: [0x11u8; 32], stake: 000_000_000 },
+            RosterMember{ pub_key: [0x22u8; 32], stake: 000_000_000 },
+            RosterMember{ pub_key: [0x33u8; 32], stake: 000_000_000 },
         ];
     }
 
