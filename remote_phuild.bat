917bcf272315e397cb7a61acc79820ffce6f9ce0
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2026-01-14 19:07:20 +0400

    Dev: Misc build cfg

--- remote_phuild.bat
+++ remote_phuild.bat
@@ -1,1 +1,7 @@
-ssh Madina@172.20.10.2 "cmd /c \"C:\Work\ShieldedLabs\crosslink_monolith\server_phuild.bat %*\" "
+@echo off
+
+set "bin=ShieldedLabs\crosslink_monolith\zebra-crosslink\target\debug\deps"
+
+ssh Madina@madina-a16.shire-lydian.ts.net cmd /c \"C:\Work\ShieldedLabs\crosslink_monolith\server_phuild.bat %*\" || exit /b
+REM copy /y "\\madina-a16.shire-lydian.ts.net\%bin%\zebrad.exe" "C:\Work\%bin%\zebrad.exe"                            || exit /b
+REM copy /y "\\madina-a16.shire-lydian.ts.net\%bin%\zebrad.pdb" "C:\Work\%bin%\zebrad.pdb"                            || exit /b
