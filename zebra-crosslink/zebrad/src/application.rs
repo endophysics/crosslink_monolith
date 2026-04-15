a3f959ec3f3c42a17ab00a06df518b199393d153
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2026-04-15 21:06:40 +0400

    Disable internal mining for now

--- zebra-crosslink/zebrad/src/application.rs
+++ zebra-crosslink/zebrad/src/application.rs
@@ -232,7 +232,7 @@ impl Application for ZebradApp {
                     //     ZcashAddress::try_from_encoded("tmRB9AEVsxNAQsqtJPqJUje9KCaAijpS77z")
                     //         .unwrap(),
                     // );
-                    c.mining.internal_miner = true;
+                    c.mining.internal_miner = false;
                     c.network.network = testnet::Parameters::build()
                         //.with_network_name("CrosslinkTestnet")
                         .with_network_magic(Magic([67, 108, 84, 48]))
